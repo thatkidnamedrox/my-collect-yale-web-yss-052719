c9f2eb3eb060f84158fec7243901d0af879dbbe0
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 12:08:40 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  if block_given?
+    i = 0
 
+    result = []
+    while i < array.length
+      result << yield(array[i])
+      i+=1
+    end
 
+    result
+  else
+    nil
+  end
+end
